62ebc97d2ecc46e0d64a95393cf77d25425ab1de
deepspacediver <luka.koscik@gmail.com>
2022-11-02 18:03:47 +0100

    mergeSort now properly concats arrays

--- mergeSort.js
+++ mergeSort.js
@@ -1,15 +1,27 @@
-let array = [7,2,3,4];
+let someArray = [4, 2,1,7,5,2];
 
-const mergeSort = (arr, i = 0, j = 0, k = 0) => {
-  if (arr.length ===1) {
+const mergeSort = (arr, arrLow = 0, arrHigh = arr.length) => {
+  if (arr.length < 2) {
     console.log(arr);
     return arr;
+  } else if (arrLow < arrHigh) {
+    console.log("hig", arrHigh, "lol", arrLow);
+    let arrMiddle = Math.round((arrLow + arrHigh) / 2);
+    let arrLeft = arr.slice(arrLow, arrMiddle);
+    let arrRight = arr.slice(arrMiddle, arrHigh);
+    console.log({arrLeft, arrRight})
+    return [...mergeSort(arrLeft)].concat([...mergeSort(arrRight)])
   }
-    let lengthOfArr = arr.length;
-    let middleOfArray = Math.round(lengthOfArr / 2);
-    let item1 = array.slice(0, middleOfArray);
-    let item2 = array.slice(middleOfArray)
-    return item1.concat(item2)
 };
 
-console.log(mergeSort(array) )
+console.log(mergeSort(someArray));
+
+const merge = (leftArr, rightArr) => {
+  let [i, j, k] = [0, 0, 0];
+  let leftLength = leftArr.length;
+  let rightLength = rightArr.length;
+  let returnedArr = [];
+};
+let arr1 = [2, 6, 4, 1];
+
+//console.log(test(arr1));
